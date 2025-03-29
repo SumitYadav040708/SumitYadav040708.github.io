75532bcf52f1a6c61cfabba6e30783b1eaf97a8b
SumitYadav040708 <sumit.yadav.040708@gmail.com>
2025-03-29 17:47:51 +0530

    Update script1.js

--- projects/project1/script1.js
+++ projects/project1/script1.js
@@ -1,1 +1,18 @@
 
+// Project 1 JavaScript
+document.addEventListener("DOMContentLoaded", () => {
+  // CRUD logic for movies list
+  const movieInput = document.getElementById("movie-input");
+  const addBtn = document.getElementById("add-btn");
+  const movieList = document.getElementById("movie-list");
+
+  let movies = JSON.parse(localStorage.getItem("movies")) || [];
+  
+  function renderMovies() {
+    movieList.innerHTML = movies.map((movie, index) => `
+      <div>${movie} <button onclick="removeMovie(${index})">X</button></div>
+    `).join("");
+  }
+  
+  // ... (rest of Project 1 JS)
+});
